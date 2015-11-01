3f3f967e632e92aa89c9a046dc4931fd5e49c61d
amrutavidwans <Amruta@lawn-143-215-58-100.lawn.gatech.edu>
2015-10-31 22:57:07 -0400

    final cross validate code

--- GenreClassification/TestCrossValidation.m
+++ GenreClassification/TestCrossValidation.m
@@ -5,30 +5,11 @@
 close all;
 clear all;
 clc;
-
-nof=10;
+nof=6;
 K=3;
 features=rand(10,500);
 chk=ones(1,100);
 GroundTruth=[chk,2*chk,3*chk,4*chk,5*chk];
 [classAccuracy, TotalAccuracy, ConfusionMatrix]=CrossValidateNFolds(K, features, GroundTruth, nof);
-
-% sequential forward selection.. nedds to be evaluated with 10 fold cross validation
-% number of features
-[numFeat,~]=size(features);
-% FeatComb
-
-iter=1;
-Hierarchy=10:-1:1;
-FeatCombNum=cell(sum(1:numFeat),1);
-FeatCombAccu=cell(sum(1:numFeat),1);
-if iter<=numFeat
-    
-    for i=1:numFeat
-        [~, TotalAccuracy, ~]=CrossValidateNFolds(K, features, GroundTruth, nof);
-    
-    end
-    iter=iter+1;
-end
 
 % end
