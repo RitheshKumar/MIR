d9d4d4ac69c424d780bef86030e261f9e079125d
Rithesh Kumar Ravikumar <rithesh@gatech.edu>
2015-11-03 23:19:43 -0500

    FeatureExtraction Function
    Meant for Q1. Need to add code for plotting features!

---
+++ GenreClassification/featureExtraction.m
@@ -0,0 +1,42 @@
+%% Function: featureExtraction 
+%  featureExtraction (windowSize, hopSize)
+% input: 
+%   windowSize: int, number of samples per block
+%   hopSize: int, number of samples per hop
+% output: 
+%   plots, visualization
+
+function featureExtraction( )
+
+	%[metaData , genre ] = getMetaData ( windowSize, hopSize)
+	load ( 'metaData.mat' );
+
+	numFiles    = length ( metaData (1,:) );
+	numFeatures = length ( metaData (:,1) );
+	
+	zScoreData  = zeros ( size (metaData) );
+
+	% z-score evaluation
+	for i = 1 : numFeatures 
+
+		zScoreData(i,:) = ( metaData(i,:) - mean( metaData(i,:) ) )./std( metaData(i,:) );
+
+	end
+
+	% finding Covariance Matrix	
+	covMat  =  zeros ( numFeatures , numFeatures );
+
+	for jj = 1 : numFeatures
+
+		for kk = 1 : numFeatures
+
+			covMat (jj,kk) = sum ( zScoreData (jj,:) .* zScoreData (kk,:) ) / (numFiles - 1);
+
+		end
+
+	end
+end
+
+	
+
+
