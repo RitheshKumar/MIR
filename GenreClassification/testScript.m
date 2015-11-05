0c15ed65224d5d3bca0fddc517e698ce0cf7e048
Rithesh Kumar Ravikumar <rithesh@gatech.edu>
2015-11-05 00:13:26 -0500

    TestScript updation
    Just your regular script to incorporate new fns.

--- GenreClassification/testScript.m
+++ GenreClassification/testScript.m
@@ -2,13 +2,28 @@ clear all;
 close all;
 clc;
 
-[audio,sampleFreq] = audioread('/Users/Rithesh/Documents/3rd Sem/TONAS/Deblas/10-D_TalegondeCordoba.wav');
+%[audio,sampleFreq] = audioread('/Users/Rithesh/Documents/3rd Sem/TONAS/Deblas/10-D_TalegondeCordoba.wav');
+%
+%windowSize = 2048; hopSize = 1024;
+%noverlap = windowSize - hopSize; nfft = windowSize*5;
+%
+%[specMat,yFreq,xTime]=spectrogram(audio,windowSize,noverlap,nfft,sampleFreq);
+%
+%specCentroid  = mySpectralCentroid (specMat) ; 
 
-windowSize = 2048; hopSize = 1024;
-noverlap = windowSize - hopSize; nfft = windowSize*5;
+load ('metaData.mat');
 
-[specMat,yFreq,xTime]=spectrogram(audio,windowSize,noverlap,nfft,sampleFreq);
+numFolds      = 10;
+featureMatrix = metaData;
+iterIdx       = 1;
+K             = 5;
 
-specCentroid  = mySpectralCentroid (specMat) ; 
+numGenres     = 5;
+genre         = 1:numGenres;
+numFiles      = length ( featureMatrix (1,:) );
+genre         = repmat  (genre, [numFiles/numGenres,1]);
+genre         = reshape (genre, [1, numFiles] );
 
+[testData, trainData, trainLabel] = getTestSet (numFolds, featureMatrix, genre, iterIdx);
+[estimatedClass]                  = myKNN (testData, trainData, trainLabel, K);
 
